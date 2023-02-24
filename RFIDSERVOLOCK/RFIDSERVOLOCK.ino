4ab4389527bde2ea1e6c76b77fbf9194fd3edf9b
Maniyar Md Ammar <123905641+ammarece@users.noreply.github.com>
2023-02-25 03:59:32 +0530

    Add files via upload

---
+++ RFIDSERVOLOCK/RFIDSERVOLOCK.ino
@@ -0,0 +1,79 @@
+/*
+Code Name:RFID DOOR LOCK
+Author: ammarece
+Description: This program is used for making RFID based door lock
+License: Remixing or Changing this Thing is allowed. Commercial use is not allowed.
+*/
+#include <Servo.h>
+#include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+String UID = ""; //Card Number
+byte lock = 0;
+
+Servo servo;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+
+void setup() {
+  Serial.begin(9600);
+  servo.write(70);
+  lcd.init();
+  lcd.backlight();
+  servo.attach(3);
+  SPI.begin();
+  rfid.PCD_Init();
+}
+
+void loop() {
+  lcd.setCursor(4, 0);
+  lcd.print("Welcome!");
+  lcd.setCursor(1, 1);
+  lcd.print("Put your card");
+
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+  if ( ! rfid.PICC_ReadCardSerial())
+    return;
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Scanning");
+  Serial.print("NUID tag is :");
+  String ID = "";
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    lcd.print(".");
+    ID.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    ID.concat(String(rfid.uid.uidByte[i], HEX));
+    delay(300);
+  }
+  ID.toUpperCase();
+
+  if (ID.substring(1) == UID && lock == 0 ) {
+    servo.write(70);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Door is locked");
+    delay(1500);
+    lcd.clear();
+    lock = 1;
+  } else if (ID.substring(1) == UID && lock == 1 ) {
+    servo.write(160);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Door is open");
+    delay(1500);
+    lcd.clear();
+    lock = 0;
+  } else {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Wrong card!");
+    delay(1500);
+    lcd.clear();
+  }
+}
